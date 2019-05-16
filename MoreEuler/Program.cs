07cb29877f2cc9c6e4db22129537aabb882f8cf1
Hilary Sasso-Schleh <hilary.sasso-schleh@accenture.com>
2019-05-16 15:49:36 -0400

    initial commit

---
+++ MoreEuler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
