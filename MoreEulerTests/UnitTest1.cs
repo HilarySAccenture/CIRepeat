e38055b29ef39b740abcf47b781fe2adb78829cf
Hilary Sasso-Schleh <hilary.sasso-schleh@accenture.com>
2019-05-17 10:03:53 -0400

    first test

--- MoreEulerTests/UnitTest1.cs
+++ MoreEulerTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using Shouldly;
 
 namespace MoreEulerTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void NumberIsEven()
         {
+            var smallestMultiple = new SmallestMultiple();
 
+            smallestMultiple.isEven(1).ShouldBeFalse();
+
+
+
+        }
+    }
+
+    public class SmallestMultiple
+    {
+        public bool isEven(int n)
+        {
+            return false;
         }
     }
 }
